aaba9c30237994e3a66f03644e28bdee24e47402
syahrul35 <irul.syahrulmaulana@gmail.com>
2024-03-07 20:56:50 +0700

    add update transaction

--- resources/js/Pages/Transactions/EditTransactionModal.vue
+++ resources/js/Pages/Transactions/EditTransactionModal.vue
@@ -20,6 +20,7 @@
                             <!-- Form tambahan -->
                             <form @submit.prevent="submitForm">
                                 <!-- Input kategori yang disaring berdasarkan jenis transaksi yang dipilih -->
+                                <input type="text" v-model="form.idUser" hidden>
                                 <div class="mt-4 sm:flex sm:items-center">
                                     <label for="transactionCategory"
                                         class="block text-sm font-medium text-gray-700 sm:w-1/4 grid justify-start grid justify-items-start">Category</label>
@@ -54,10 +55,9 @@
                                         class="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm sm:w-3/4">
                                 </div>
                                 <div class="mt-4 sm:flex sm:items-center">
-                                    <span class="block text-sm font-medium text-gray-700 justify-start ">Is This
-                                        Transaction Used ATM?</span>
+                                    <span class="block text-sm font-medium text-gray-700 justify-start ">Is This Transaction Used ATM?</span>
                                     <input type="checkbox" id="transactionIsATM" name="transactionIsATM"
-                                        v-model="form.isATM" class="mx-2 rounded-sm">
+                                        v-model="form.isATM" :checked="form.isATM" class="mx-2 rounded-sm">
                                 </div>
                             </form>
                         </div>
@@ -79,67 +79,87 @@
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits, computed } from 'vue';
-import { useForm } from '@inertiajs/vue3';
-import SelectType from '../../Components/SelectType.vue';
+    import { ref, defineProps, defineEmits, computed, onMounted } from 'vue';
+    import { useForm } from '@inertiajs/vue3';
+    import SelectType from '../../Components/SelectType.vue';
 
-const props = defineProps({
-    categories: Array,
-    userId: String,
-});
+    const props = defineProps({
+        categories: Array,
+        userId: String,
+        selectedTransaction: Object,
+    });
 
-// Modal
-const emits = defineEmits(['closeModal']);
+    // Modal
+    const emits = defineEmits(['closeModal']);
 
-const selectedType = ref('income'); // Menyimpan jenis transaksi yang dipilih
+    const selectedType = ref('income');
 
-const closeModal = () => {
-    emits('closeModal');
-};
-// end Modal
+    const closeModal = () => {
+        emits('closeModal');
+    };
+    // end Modal
 
-// Filter categories based on selectedType
-const filteredCategories = computed(() => {
-    return props.categories.filter(category => category.type === selectedType.value);
-});
+    // Computed
+    const formattedTotal = computed({
+        get: () => {
+            // Menggunakan regex untuk menambahkan titik sebagai pemisah ribuan saat menampilkan nilai
+            return form.total.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+        },
+        set: (newValue) => {
+            // Menghapus titik dari nilai yang dimasukkan ke dalam form saat disimpan
+            form.total = newValue.replace(/\D/g, '');
+        }
+    })
 
-const form = useForm({
-    idUser: userId,
-    idCategory: '',
-    date: '',
-    total: '',
-    desc: '',
-    isATM: ''
-})
+    // Filter categories based on selectedType
+    const filteredCategories = computed(() => {
+        return props.categories.filter(category => category.type === selectedType.value)
+    })
 
-const formattedTotal = computed({
-    get: () => {
-        // Menggunakan regex untuk menambahkan titik sebagai pemisah ribuan saat menampilkan nilai
-        return form.total.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-    },
-    set: (newValue) => {
-        // Menghapus titik dari nilai yang dimasukkan ke dalam form saat disimpan
-        form.total = newValue.replace(/\D/g, '');
+    // Form Edit
+    const editedTransaction = ref({
+        id: props.selectedTransaction.id,
+        idUser: props.selectedTransaction.idUser,
+        idCategory: props.selectedTransaction.idCategory,
+        date: props.selectedTransaction.date,
+        total: props.selectedTransaction.total,
+        desc: props.selectedTransaction.desc,
+        isATM: props.selectedTransaction.isATM,
+    })
+
+    const form = useForm({
+        idUser: editedTransaction.value.idUser,
+        idCategory: editedTransaction.value.idCategory,
+        date: editedTransaction.value.date,
+        total: editedTransaction.value.total,
+        desc: editedTransaction.value.desc,
+        isATM: editedTransaction.value.isATM,
+    })
+    // End Form Edit
+
+    // Mounted
+    onMounted(() => {
+        form.idCategory = props.selectedTransaction.idCategory
+        form.date = props.selectedTransaction.date
+        form.total = props.selectedTransaction.total
+        form.desc = props.selectedTransaction.desc
+        form.isATM = !!props.selectedTransaction.isATM
+    })
+
+    const submitForm = async () => {
+        try {
+            await form.put(route('transactions.update', { transaction: editedTransaction.value.id }), {
+                idUser: editedTransaction.value.idUser,
+                idCategory: editedTransaction.value.idCategory,
+                date: editedTransaction.value.date,
+                total: editedTransaction.value.total.replace(/\D/g, ''),
+                desc: editedTransaction.value.desc,
+                isATM: editedTransaction.value.isATM
+            })
+            closeModal()
+
+        } catch (error) {
+            console.log(error)
+        }
     }
-})
-
-const submitForm = async () => {
-    try {
-        // form.isATM = document.getElementById('transactionIsATM').checked;
-        console.log('cek', form.total.replace(/\D/g, ''))
-
-        await form.post(route('transactions.store'), {
-            idUser: form.idUser,
-            idCategory: form.idCategory,
-            date: form.date,
-            total: form.total.replace(/\D/g, ''),
-            desc: form.desc,
-            isATM: form.isATM
-        })
-        closeModal()
-
-    } catch (error) {
-        console.log(error)
-    }
-}
 </script>

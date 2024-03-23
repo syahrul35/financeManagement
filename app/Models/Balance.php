32b835c47d0d05fa8bc82e0343be37b852e31a85
syahrul35 <irul.syahrulmaulana@gmail.com>
2024-03-23 13:53:22 +0700

    add balance table

---
+++ app/Models/Balance.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Balance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'idUser',
+        'balance',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'idUser');
+    }
+}
